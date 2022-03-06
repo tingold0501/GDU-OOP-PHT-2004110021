88fc20ae4271a6e2689835afb075832c39c0f201
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2022-03-06 23:06:40 +0700

    Bài Tập Tuần 3 Buổi 6

--- PHT-2004110021-K14DCPM02/src/com/tinph2004110021/baitapvenhaBuoi6/Account.java
+++ PHT-2004110021-K14DCPM02/src/com/tinph2004110021/baitapvenhaBuoi6/Account.java
@@ -4,4 +4,36 @@ public class Account {
     private long soTk;
     private String tenTk;
     private double soTien;
+    
+    @Override
+    public String toString() {
+        return "Account [soTien=" + soTien + ", soTk=" + soTk + ", tenTk=" + tenTk + "]";
+    }
+    public Account(long soTk, String tenTk, double soTien) {
+        this.soTk = soTk;
+        this.tenTk = tenTk;
+        this.soTien = soTien;
+    }
+    public Account(){
+
+    }
+    public long getSoTk() {
+        return soTk;
+    }
+    public void setSoTk(long soTk) {
+        this.soTk = soTk;
+    }
+    public String getTenTk() {
+        return tenTk;
+    }
+    public void setTenTk(String tenTk) {
+        this.tenTk = tenTk;
+    }
+    public double getSoTien() {
+        return soTien;
+    }
+    public void setSoTien(double soTien) {
+        this.soTien = soTien;
+    }
+   
 }
