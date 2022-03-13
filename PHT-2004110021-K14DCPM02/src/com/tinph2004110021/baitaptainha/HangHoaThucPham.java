455345b452bbc98427a4e45abe304ee70a36af43
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2022-03-13 23:21:11 +0700

    Test Bài Của Thầy

---
+++ PHT-2004110021-K14DCPM02/src/com/tinph2004110021/baitaptainha/HangHoaThucPham.java
@@ -0,0 +1,59 @@
+package com.tinph2004110021.baitaptainha;
+
+import java.util.Date;
+
+public class HangHoaThucPham {
+    private int idHang;
+    private static int autoIdHang = 1;
+    private String nameHang;
+    private double donGiaHang;
+    private Date ngaySx, ngayHh;
+    public int getIdHang() {
+        return idHang;
+    }
+    private void setIdHang(int idHang) {
+        this.idHang = idHang;
+    }
+    public String getNameHang() {
+        return nameHang;
+    }
+    private void setNameHang(String nameHang) {
+        this.nameHang = nameHang;
+    }
+    public double getDonGiaHang() {
+        return donGiaHang;
+    }
+    private void setDonGiaHang(double donGiaHang) {
+        this.donGiaHang = donGiaHang;
+    }
+    public Date getNgaySx() {
+        return ngaySx;
+    }
+    private void setNgaySx(Date ngaySx) {
+        this.ngaySx = ngaySx;
+    }
+    public Date getNgayHh() {
+        return ngayHh;
+    }
+    private void setNgayHh(Date ngayHh) {
+        this.ngayHh = ngayHh;
+    }
+    public HangHoaThucPham(int idHang) {
+        this.idHang = idHang;
+    }
+    public HangHoaThucPham( String nameHang, double donGiaHang, Date ngaySx, Date ngayHh) {
+        this.idHang = autoIdHang++;
+        this.nameHang = nameHang;
+        this.donGiaHang = donGiaHang;
+        this.ngaySx = ngaySx;
+        this.ngayHh = ngayHh;
+    }
+    @Override
+    public String toString() {
+        return "Hàng Hoá Thực Phẩm [Đơn Giá Hàng =" + donGiaHang + 
+        ", Mã Hàng =" + idHang + 
+        ", Tên Hàng Hoá =" + nameHang + 
+        ", Ngày Hết Hạn =" + ngayHh + 
+        ", Ngày Sản Xuất =" + ngaySx + "]";
+    }
+}
---
+++ PHT-2004110021-K14DCPM02/src/com/tinph2004110021/baitaptainha/HangHoaThucPhamTestDrive.java
@@ -0,0 +1,7 @@
+package com.tinph2004110021.baitaptainha;
+
+public class HangHoaThucPhamTestDrive {
+    public static void main(String[] args) {
+        
+    }
+}
