31ca6cdb9234221c48e21dad03e53377d155a614
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2022-03-20 23:38:38 +0700

    11:38 PM 20/03/2022

---
+++ PHT-2004110021-K14DCPM02/src/com/tinph2004110021/baitaptainha/GiaoDichDat.java
@@ -0,0 +1,33 @@
+package com.tinph2004110021.baitaptainha;
+
+public class GiaoDichDat extends GiaoDich {
+    private String loaiDat;
+ 
+    public GiaoDichDat() {
+        super();
+    }
+ 
+    public GiaoDichDat(String loaiDat) {
+        super();
+        this.loaiDat = loaiDat;
+    }
+ 
+    public String getLoaiDat() {
+        return loaiDat;
+    }
+ 
+    public void setLoaiDat(String loaiDat) {
+        this.loaiDat = loaiDat;
+    }
+     
+    public void nhap() {
+        super.nhap();
+        System.out.print("Nhập loại đất (A/B/C): ");
+        loaiDat = scanner.nextLine();
+    }
+ 
+    @Override
+    public String toString() {
+        return super.toString() + ", loại đất: " + this.loaiDat;
+    }
+}
