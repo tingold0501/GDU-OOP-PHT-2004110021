fc33719e9885e4570a717b27b830a28e6163020f
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2022-03-22 21:37:51 +0700

    9:37 PM 22/03/2022

--- PHT-2004110021-K14DCPM02/src/com/tinph2004110021/SuaBaiHoaDon/DanhSachHoaDon.java
+++ PHT-2004110021-K14DCPM02/src/com/tinph2004110021/SuaBaiHoaDon/DanhSachHoaDon.java
@@ -1,16 +1,26 @@
 package com.tinph2004110021.SuaBaiHoaDon;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 public class DanhSachHoaDon {
-   HoaDon danSach[] = new HoaDon[10];
+   HoaDon danhSach[] = new HoaDon[10];
+   List<HoaDon> lDons = new ArrayList<>();
    Scanner sc = new Scanner(System.in);
    protected void nhap(HoaDon hoaDon){
        System.out.println("Nhập Đơn Giá:");
+       double donGia = sc.nextDouble();
        System.out.println("Nhập Mã Hoá Đơn:");
+       String maHoaDon = sc.nextLine();
        System.out.println("Nhập Mã Phòng:");
+       String maPhong = sc.nextLine();
        System.out.println("Nhập Ngày Hoá Đơn:");
-       System.out.println("Nhập Tên Khách Hàng:");
+       
+    //    System.out.println("Nhập Tên Khách Hàng:");
+    //    String tenKhachHang = sc.nextLine();
+    //    HoaDon hoaDon2 = new HoaDon(maHoaDon,tenKhachHang,maPhong,donGia);
    }
    protected void xuat(){
        
